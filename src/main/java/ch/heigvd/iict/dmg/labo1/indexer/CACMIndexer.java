3b0baead2fc1c88d32a2031f420409e192902bef
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-10-14 16:28:56 +0200

    avancement labo

--- src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
+++ src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
@@ -2,16 +2,23 @@ package ch.heigvd.iict.dmg.labo1.indexer;
 
 import ch.heigvd.iict.dmg.labo1.parsers.ParserListener;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.document.Document;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexableFieldType;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class CACMIndexer implements ParserListener {
@@ -49,6 +56,48 @@ public class CACMIndexer implements ParserListener {
 	public void onNewDocument(Long id, String authors, String title, String summary) {
 		Document doc = new Document();
 
+		Field idField      = new StoredField("id", id);
+		Field authorsField = new StringField("authors", authors,Field.Store.YES);
+		Field titleField   = new StringField("title",title,Field.Store.YES);
+		Field summaryField = new TextField("summary", summary, Field.Store.YES);
+
+		/*try (InputStream stream = Files.newInputStream(file)) {
+
+			// Add the path of the file as a field named "path".  Use a
+			// field that is indexed (i.e. searchable), but don't tokenize
+			// the field into separate words and don't index term frequency
+			// or positional information:*/
+			doc.add(idField);
+			doc.add(authorsField);
+			doc.add(titleField);
+			doc.add(summaryField);
+/*
+			// Add the last modified date of the file a field named "modified".
+			// Use a LongPoint that is indexed (i.e. efficiently filterable with
+			// PointRangeQuery).  This indexes to milli-second resolution, which
+			// is often too fine.  You could instead create a number based on
+			// year/month/day/hour/minutes/seconds, down the resolution you require.
+			// For example the long value 2011021714 would mean
+			// February 17, 2011, 2-3 PM.
+			doc.add(new LongPoint("modified", lastModified));
+
+			// Add the contents of the file to a field named "contents".  Specify a Reader,
+			// so that the text of the file is tokenized and indexed, but not stored.
+			// Note that FileReader expects the file to be in UTF-8 encoding.
+			// If that's not the case searching for special characters will fail.*/
+			//doc.add(new TextField("contenu", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+/*
+			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+				// New index, so we just add the document (no old document can be there):
+				System.out.println("adding " + file);
+				writer.addDocument(doc);
+			} else {
+				// Existing index (an old copy of this document may have been indexed) so
+				// we use updateDocument instead to replace the old one matching the exact
+				// path, if present:
+				System.out.println("updating " + file);
+				writer.updateDocument(new Term("path", file.toString()), doc);
+			}*/
 		// TODO student: add to the document "doc" the fields given in
 		// parameters. You job is to use the right Field and FieldType
 		// for these parameters.
