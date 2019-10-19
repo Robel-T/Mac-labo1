7205676326f5be787fccf6e3f24685bd2a56c4f1
Jael24 <jael.dubey@heig-vd.ch>
2019-10-19 15:51:47 +0200

    Ajout de l'index

--- src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
+++ src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
@@ -54,53 +54,20 @@ public class CACMIndexer implements ParserListener {
 	
 	@Override
 	public void onNewDocument(Long id, String authors, String title, String summary) {
+		// Make a new empty document
 		Document doc = new Document();
 
-		Field idField      = new StoredField("id", id);
+		// Create all fields for all informations
+		Field storeIdField = new StoredField("storedId", id);
 		Field authorsField = new StringField("authors", authors,Field.Store.YES);
-		Field titleField   = new StringField("title",title,Field.Store.YES);
-		Field summaryField = new TextField("summary", summary, Field.Store.YES);
+		Field titleField = new StringField("title",title,Field.Store.YES);
 
-		/*try (InputStream stream = Files.newInputStream(file)) {
+		// The summary is not stored
+		doc.add(new TextField("summary", summary, Field.Store.NO));
 
-			// Add the path of the file as a field named "path".  Use a
-			// field that is indexed (i.e. searchable), but don't tokenize
-			// the field into separate words and don't index term frequency
-			// or positional information:*/
-			doc.add(idField);
-			doc.add(authorsField);
-			doc.add(titleField);
-			doc.add(summaryField);
-/*
-			// Add the last modified date of the file a field named "modified".
-			// Use a LongPoint that is indexed (i.e. efficiently filterable with
-			// PointRangeQuery).  This indexes to milli-second resolution, which
-			// is often too fine.  You could instead create a number based on
-			// year/month/day/hour/minutes/seconds, down the resolution you require.
-			// For example the long value 2011021714 would mean
-			// February 17, 2011, 2-3 PM.
-			doc.add(new LongPoint("modified", lastModified));
-
-			// Add the contents of the file to a field named "contents".  Specify a Reader,
-			// so that the text of the file is tokenized and indexed, but not stored.
-			// Note that FileReader expects the file to be in UTF-8 encoding.
-			// If that's not the case searching for special characters will fail.*/
-			//doc.add(new TextField("contenu", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
-/*
-			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-				// New index, so we just add the document (no old document can be there):
-				System.out.println("adding " + file);
-				writer.addDocument(doc);
-			} else {
-				// Existing index (an old copy of this document may have been indexed) so
-				// we use updateDocument instead to replace the old one matching the exact
-				// path, if present:
-				System.out.println("updating " + file);
-				writer.updateDocument(new Term("path", file.toString()), doc);
-			}*/
-		// TODO student: add to the document "doc" the fields given in
-		// parameters. You job is to use the right Field and FieldType
-		// for these parameters.
+		doc.add(storeIdField);
+		doc.add(authorsField);
+		doc.add(titleField);
 
 		try {
 			this.indexWriter.addDocument(doc);
